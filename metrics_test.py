8b40fca9f606168cad0d7c6d1fc3e83183114da1
burkel <louis.burke@hmhco.com>
2021-12-10 18:04:34 +0000

    Adds more unittests.

--- metrics_test.py
+++ metrics_test.py
@@ -13,6 +13,46 @@ import metrics
 
 class PySparkTest(unittest.TestCase):
 
+    ROW = {
+        "id": [
+            "1c269ade-d654-4374-aafb-3deaebe5376a",
+            "1c269ade-d654-4374-aafb-3deaebe5376b",
+            "1c269ade-d654-4374-aafb-3deaebe5376c"
+        ],
+        "datetime": [
+            "2021-01-23 10:23:51",
+            "2021-01-23 10:23:52",
+            "2021-01-23 10:23:53"
+        ],
+        "domain": [
+            "www.domain-A.eu",
+            "www.domain-A.eu",
+            "www.domain-A.eu"
+        ],
+        "type": [
+            'pageview',
+            'consent.given',
+            'consent.asked'
+        ],
+        "user":[
+            {
+                "id": "1705c98b-367c-6d09-a30f-da9e6f4da700",
+                "country": "FR",
+                "token": "{\"vendors\":{\"enabled\":[],\"disabled\":[]},\"purposes\":{\"enabled\":[],\"disabled\":[]}}"
+            },
+            {
+                "id": "1705c98b-367c-6d09-a30f-da9e6f4da701",
+                "country": "FR",
+                "token": "{\"vendors\":{\"enabled\":[],\"disabled\":[]},\"purposes\":{\"enabled\":[],\"disabled\":[]}}"
+            },
+            {
+                "id": "1705c98b-367c-6d09-a30f-da9e6f4da702",
+                "country": "FR",
+                "token": "{\"vendors\":{\"enabled\":[],\"disabled\":[]},\"purposes\":{\"enabled\":[],\"disabled\":[]}}"
+            }
+        ]
+    }
+
     @classmethod
     def suppress_py4j_logging(cls):
         logger = logging.getLogger('py4j')
@@ -104,57 +144,16 @@ class PySparkTest(unittest.TestCase):
 
 
     def test_distinct_types(self):
-        row = {
-            "id": [
-                "1c269ade-d654-4374-aafb-3deaebe5376a",
-                "1c269ade-d654-4374-aafb-3deaebe5376b",
-                "1c269ade-d654-4374-aafb-3deaebe5376c"
-            ],
-            "datetime": [
-                "2021-01-23 10:23:51",
-                "2021-01-23 10:23:52",
-                "2021-01-23 10:23:53"
-            ],
-            "domain": [
-                "www.domain-A.eu",
-                "www.domain-A.eu",
-                "www.domain-A.eu"
-            ],
-            "type": [
-                'pageview',
-                'consent.given',
-                'consent.asked'
-            ],
-            "user":[
-                {
-                    "id": "1705c98b-367c-6d09-a30f-da9e6f4da700",
-                    "country": "FR",
-                    "token": "{\"vendors\":{\"enabled\":[],\"disabled\":[]},\"purposes\":{\"enabled\":[],\"disabled\":[]}}"
-                },
-                {
-                    "id": "1705c98b-367c-6d09-a30f-da9e6f4da701",
-                    "country": "FR",
-                    "token": "{\"vendors\":{\"enabled\":[],\"disabled\":[]},\"purposes\":{\"enabled\":[],\"disabled\":[]}}"
-                },
-                {
-                    "id": "1705c98b-367c-6d09-a30f-da9e6f4da702",
-                    "country": "FR",
-                    "token": "{\"vendors\":{\"enabled\":[],\"disabled\":[]},\"purposes\":{\"enabled\":[],\"disabled\":[]}}"
-                }
-            ]
-        }
+        data_pandas = pd.DataFrame(self.ROW)
 
-        data_pandas = pd.DataFrame(row)
-
-        # Turn the data into a Spark DataFrame, self.spark comes from our PySparkTest base class
+        # Turn the data into a Spark DataFrame
         data_spark = self.spark.createDataFrame(data_pandas)
         deduplicated_data = metrics.deduplicate_frame(data_spark, 'id')
         deduplicated_data.createOrReplaceTempView("Events")
 
-        # Invoke the unit we'd like to test
+        # The unit test.
         results_spark_list = metrics.get_distinct_types(self.spark)
 
-        # Turn the results back to Pandas
         expected_list = [
             'pageview',
             'consent.given',
@@ -163,6 +162,16 @@ class PySparkTest(unittest.TestCase):
 
         # Assert that the 2 results are the same. 
         self.assertEqual(set(expected_list), set(results_spark_list))
+
+
+    def test_get_event_type_metrics(self):
+        data_pandas = pd.DataFrame(self.ROW)
+
+        # Turn the data into a Spark DataFrame
+        data_spark = self.spark.createDataFrame(data_pandas)
+        data_spark.createOrReplaceTempView("Events")
+
+        metric_frame = metrics.get_event_type_metrics('pageview', self.spark)
 
 
 if __name__ == "__main__":
